e625ea2bc86af79595cb484919b534f6c678f820
kevlut <kevinluty@gmail.com>
2014-06-26 12:57:05 -0500

    Initial commit

---
+++ Sandbox.WPF/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="KCL">
+//     Copyright (c) KCL. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Sandbox.WPF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
